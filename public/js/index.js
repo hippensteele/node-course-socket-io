5a8a04439e06c565428b07b59d1336a55e23832d
Tom <hippensteele@gmail.com>
2018-08-31 17:00:18 +0000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -24,13 +24,15 @@ socket.on('disconnect', function() {
     console.log('Disonnected from the server');
 });
 
+var messageTextbox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function (data) {
-        console.log('From server', data);
+        messageTextbox.val('');
     });
 });
 
@@ -39,12 +41,15 @@ locationButton.on('click', function() {
     if (!navigator.geolocation){
         return alert('Geolocation not supported by your browser');
     };
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
